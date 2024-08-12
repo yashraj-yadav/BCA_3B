665225214124e2ad5520abb15a676492b9a63cf7
yashraj-yadav <yadavyash9760@gmail.com>
2024-08-12 11:36:47 +0530

    backticks

---
+++ file1.js
@@ -0,0 +1,75 @@
+// save file with file1.js
+// alt+shift is use for duplicate the line below
+document.write("Hello from JS</br>");
+document.write("Hello from JS</br>");
+
+// var x  == declaration
+// var x=10; initialization
+// var x=10; definition
+
+// document.write("addition is ",x+y, "</br>")
+// document.write("subtraction is ",x-y, "</br>")
+// document.write("multiplication is ",x*y, "</br>")
+// document.write("division is ",x/y, "</br>")
+
+// console.log("Hello this is js");
+
+// var x =120;
+// function xyz(){
+//     var y = 30;
+// }
+// console.log(x,y);
+
+// let x=20;
+// {
+//     let y=30;
+// }
+// console.log(x,y);
+
+// const x=12;
+
+let a=10;
+document.write(a,"<br>")
+let b=5;
+document.write(b,"<br>")
+let sum=a+b;
+document.write(sum,"<br>")
+let difference = a-b;
+document.write(difference,"<br>")
+let product=a*b;
+document.write(product,"<br>")
+let quotient=a/b;
+document.write(quotient,"<br>")
+let remainder= a%b;
+document.write(remainder,"<br>")
+let isEqual=(a==b);
+document.write(isEqual,"<br>")
+let isNotEqual=(a!=b);
+document.write(isNotEqual,"<br>")
+let isGreater=(a>b);
+document.write(isGreater,"<br>")
+let logicalAnd=(a>b && b<10);
+document.write(logicalAnd,"<br>")
+
+let fname="John";
+document.write(`Hello ${fname} <br>`)
+
+let isStudent=false;
+document.write(`student ${isStudent ? "yes" : "no"} <br>`)
+
+
+
+
+let age=25;
+// if (age>=18) {
+//     document.write("You are eligible to vote")
+    
+// } 
+// else {
+
+//     document.write("You are not eligible to vote")
+// }
+
+document.write(age>=18 ? "you r eligible" : "not eligible")
+
+
